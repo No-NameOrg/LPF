fc6e32c38efcff0f8e7f48599f2d75e472070b61
Govind-Asawa <govindasawa23@gmail.com>
2021-02-21 15:23:08 +0530

    Necessary fixes to Wrapper classes

--- Flask/DecisionTreeWrapper.py
+++ Flask/DecisionTreeWrapper.py
@@ -19,41 +19,37 @@ class DecisionTreeWrapper:
         if not os.path.exists(self.__subfolder):
             os.mkdir(f'.\\{self.__subfolder}')
 
-    def train(self):
-        COLS_TO_REMOVE = []
-        with open(r'.\cols_to_remove.txt') as file:
-            COLS_TO_REMOVE = file.read().replace(" ","").split(",")
+    def train(self, data = pd.DataFrame(), y = None):
         
-        data = pd.read_csv(r'.\data\loan-dataset.csv')
-        X,y = data.drop(COLS_TO_REMOVE + ['loanid','loanstatus'], axis=1), data.loc[:,'loanstatus'].map({'Y':0,'N':1}).astype('uint8')
+        dump = False
+        if data.size == 0:
+            dump = True
+            COLS_TO_REMOVE = []
+            with open(r'.\cols_to_remove.txt') as file:
+                COLS_TO_REMOVE = file.read().replace(" ","").split(",")
+            
+            data = pd.read_csv(r'.\data\loan-dataset.csv')
+            data,y = data.drop(COLS_TO_REMOVE + ['loanid','loanstatus'], axis=1), data.loc[:,'loanstatus'].map({'Y':0,'N':1}).astype('uint8')
         
-        X = self.__preProcess(X, exec_type='train')
-        
-        self.__model = DecisionTreeClassifier(criterion='gini', max_leaf_nodes=500, random_state=2, min_samples_split=0.05)
-        self.__model.fit(X, y)
-
-        pickle.dump(self.__model, open(f'..\\pickle-models\\{self.__filePrefix}-model.pkl', 'wb'))
-
-    def train(self, data, y):
-      
-        X = self.__preProcessNotDic(data, exec_type='train')
+        X = self.__preProcess(data, exec_type='train')
         
         self.__model = DecisionTreeClassifier(criterion='gini', max_leaf_nodes=500, random_state=2, min_samples_split=0.05)
         self.__model.fit(X, y)
+        
+        if dump:
+            pickle.dump(self.__model, open(f'..\\pickle-models\\{self.__filePrefix}-model.pkl', 'wb'))
 
-        # pickle.dump(self.__model, open(f'..\\pickle-models\\{self.__filePrefix}-model.pkl', 'wb'))
-    
     def score(self, X, y):
         if not self.__model :
             self.__model = pickle.load(open(f'..\\pickle-models\\{self.__filePrefix}-model.pkl','rb'))
         
-        goodX = self.__preProcessNotDic(X, 'test')
+        goodX = self.__preProcess(X, 'test',False)
 
         return self.__model.score(goodX, y)
 
     def predictProba(self, X):
 
-        data = self.__preProcessNotDic(X, 'test')
+        data = self.__preProcess(X, 'test', False)
         
         if not self.__model :
             self.__model = pickle.load(open(f'..\\pickle-models\\{self.__filePrefix}-model.pkl','rb'))
@@ -75,20 +71,12 @@ class DecisionTreeWrapper:
         noProb = self.__model.predict_proba(data)[0,1]
 
         return 'Y' if noProb <= 0.4 else 'N'
-    def __preProcess(self, X, exec_type):
+    
+    def __preProcess(self, X, exec_type, isDict = True):
 
-        data = pd.DataFrame(X, index=[0]) if exec_type == 'test' else X
+        data = pd.DataFrame(X, index=[0]) if (exec_type == 'test' and isDict) else X
 
         data.columns = list(map(lambda col: re.sub(r"\W|_","",col.lower()),data.columns))
-
-        data = dataMunging.basicMunging(data,imputeCredit_History=False)
-        
-        data = dataMunging.createDummiesExcept(data=data,filePrefix=f'{self.__subfolder}\\{self.__filePrefix}',exec_type=exec_type)
-        dataMunging.minMaxScaler(data, ['loanamount', 'loanamountterm'], filePrefix=f'{self.__subfolder}\\{self.__filePrefix}',exec_type=exec_type)
-
-        return data
-
-    def __preProcessNotDic(self, data, exec_type):
 
         data = dataMunging.basicMunging(data,imputeCredit_History=False)
         
